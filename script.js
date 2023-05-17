c2e8c4386a15aac74301c429a134b11b2612658c
Munish <hmunish5@gmail.com>
2023-05-17 23:47:25 +0200

    added toggle mobile menu functionality using javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+// DOM Elements
+const mobileNav = document.querySelector('.mobile_nav');
+const navOpenIcon = document.querySelector('.nav_icon');
+const navCloseIcon = document.querySelector('.nav_close_icon');
+const header = document.querySelector('.header_navbar');
+const banner = document.querySelector('.banner');
+// FUNCTIONS
+// Toggling Nav Function
+function toggleMobileNav() {
+  mobileNav.classList.toggle('dp-none');
+  navOpenIcon.classList.toggle('dp-none');
+  header.classList.toggle('blur');
+  banner.classList.toggle('blur');
+}
+
+// EVENT LISTNERS
+navOpenIcon.addEventListener('click', toggleMobileNav, false);
+navCloseIcon.addEventListener('click', toggleMobileNav, false);
