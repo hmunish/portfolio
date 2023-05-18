6d0990896c51b4d1b3be87076cf10fc1b6dea65e
Munish <hmunish5@gmail.com>
2023-05-18 09:02:50 +0200

    added scrolling functionality for menu links

--- script.js
+++ script.js
@@ -14,3 +14,8 @@ function toggleMobileNav() {
 }
 navOpenIcon.addEventListener('click', toggleMobileNav, false);
 navCloseIcon.addEventListener('click', toggleMobileNav, false);
+mobileNav.addEventListener('click', (e)=>{
+  if(e.target.tagName == 'LI'){
+    toggleMobileNav();
+  };
+})
